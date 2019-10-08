e9d1cf779130e6ffd950f483fb1f6c9b1ebd4a5b
Calvin McDowell <calvinmac40@hotmail.com>
2019-10-08 23:59:08 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,12 +2,13 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 require_relative "../lib/kid.rb"
-require_relative "../lib/dancer.rb"
 
 
 class Kid 
 include Dance
 extend MetaData
+include FancyDacning::InstanceMethods
+extend Fancy::ClassMethod
   attr_accessor :name
  
   def initialize(name)
