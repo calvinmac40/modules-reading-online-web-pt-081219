489beeb6fb3cb39192ebdb429864582f6ae8e98c
Calvin McDowell <calvinmac40@hotmail.com>
2019-10-08 23:52:53 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,7 +1,8 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
-
+require_relative "../lib/kid.rb"
+require_relative "../lib/dancer.rb"
 
 
 class Kid 
