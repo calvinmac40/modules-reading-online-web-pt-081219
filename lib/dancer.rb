44ca887699393bfd1649d28e03885f502135f7d0
Calvin McDowell <calvinmac40@hotmail.com>
2019-10-09 01:05:44 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -5,7 +5,10 @@ require_relative './fancy_dance.rb'
 class Dancer
   include Dance
   extend MetaDancing
-  
+  class Dancer
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+end
   attr_accessor :name 
   
   def initialize(name)
