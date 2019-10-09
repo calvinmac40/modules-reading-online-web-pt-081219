fca26308dd9123d9087437a04e6abf7052dfe83e
Calvin McDowell <calvinmac40@hotmail.com>
2019-10-09 01:08:46 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,8 +3,6 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
 class Kid
-  include Dance
-  extend MetaData
   class Kid
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
